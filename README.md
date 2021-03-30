55c99001287482100f03f934e5ffbc53ee53b6ca
wjdghks963 <74060017+wjdghks963@users.noreply.github.com>
2021-03-30 19:49:21 +0900

    s
    Initial Commit

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
-"# react" 
-"# react" 
-"# react" 
+# react 
+
--- es.js
+++ es.js
@@ -58,3 +58,43 @@ console.log(myBaby);
 
 const jung = new Human("Jung", "Hwan");
 console.log(jung);
+
+// Array map
+// days에 있는 모든 요일의 function 실행
+
+const days = ["Mon", "Tue", "Wed", "Thurs", "Fri"];
+
+const addNumber = (day, index) => `#${index + 1} ${day}`;
+
+const smilingDays = days.map(addNumber); 
+
+console.log(smilingDays); // >> #1Mon, #2Tue, #3Wed,...
+
+// Array filter 
+
+const numbers = [2,321,73,2312,321, 213,5,23,213,];
+
+const testCondition = number => number < 15;
+
+const biggerThan15 = numbers.filter(testCondition);
+
+console.log(biggerThan15);
+
+let posts = ["Hi","Hello", "Bye"];
+
+posts = posts.filter(post => post !== "Bye");
+console.log(posts);     // ["Hi", "Hello"]
+
+// forEach includes push
+
+let posts = ["Hi","Hello", "Bye"];
+
+posts.forEach(post => console.log(post))    //Hi, Hello, Bye
+
+posts.push("new")       // ["Hi", "Hello", "Bye", "new"]
+
+let greetings = ["Hi", "Howdy", "Suup"];
+
+if(!greetings.includes("Hello")){           // ["Hi", "Howdy", "Suup", "Hello"]
+    greetings.push("Hello");
+}
